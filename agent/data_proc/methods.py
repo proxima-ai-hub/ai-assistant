16e86295fbb514835002eacccdb07d3c2d2c3594
Savoskin Semyon <s.v.savoskin@gmail.com>
2024-09-13 01:01:27 +0300

    add convert method

---
+++ agent/data_proc/methods.py
@@ -0,0 +1,14 @@
+from agent.data_proc.resources import punctuation, glossary, dict_glossary
+
+
+def question_converter(question: str) -> str:
+    question = question.lower().translate(str.maketrans('', '', punctuation))
+    question_list = question.split()
+    for ind, word in enumerate(question_list):
+        if word in glossary:
+            if word == 'тк':
+                if ind < len(question_list) - 1 and question_list[ind + 1] == 'рф':
+                    question_list[ind] = dict_glossary['тк рф']
+                    continue
+            question_list[ind] = dict_glossary[word]
+    return " ".join(question_list)
